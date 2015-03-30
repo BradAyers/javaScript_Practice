4c4f3ee1c1f2f35c43ca0ed39fb2a29ff0084acf
BradAyers <brad@ayerskin.com>
2015-03-29 23:55:37 -0600

    Winner function completed. Time to add game play functions.

--- ticTacToe/scripts/index.js
+++ ticTacToe/scripts/index.js
@@ -1,58 +1,99 @@
 /** #####################################
  *  # Created by BA.Ayers on 3/26/2015. #
-    ##################################### */
+ ##################################### */
 
 // Tic Tac Toe game
 
 //  1. Initialize ticTacToe object (board)
-var board = {row1: ["x", "x", "x"], row2: [3, 4, 5], row3: [6, 7, 8]};
- var xWins = ["x", "x", "x"];
+var board = {row1: [0, 1, "O"], row2: [3, 4, "O"], row3: [6, 7, 8]};
 
-// Print board function test horizontal
-var compare = [];
-for (row = 1; row < 4; row++) {
-    var thisRow = "row" + row;
-    var arr = board[thisRow];
-    if (arr === xWins) {
-        alert("You win!")
-    }
-    else {
-        console.log(arr);
-    }
-    // board[thisRow].forEach(function(piece){
-    //    compare[row-1] = piece;
-        // console.log(thisRow, piece);
-    // })
+function testWinner() {
+    console.log(winner());
 }
 
-// Print board function test vertical
-for (idx = 0; idx < 3; idx++) { // idx are array indexes for comparing columns with same index
+function winner() {
+    if (testHor()) {return (testHor() + " wins!")};
+    if (testVert()) {return (testVert() + " wins!")};
+    if (testDiag()) {return (testDiag() + " wins!")};
+    return "No winner yet.";
+}
+
+
+// FUNCTION TO TEST HORIZONTAL ROWS FOR A WINNER (MOSTLY TESTED, OTHER THAN RETURN)
+function testHor() {
+    // Define winners
+    var xWins = ["X", "X", "X"];
+    var oWins = ["Y", "Y", "Y"];
     for (row = 1; row < 4; row++) {
-        var thisRow = "row" + row;
-    //    console.log(idx, board[thisRow][idx]);
+        //var winner =_.isEqual(board.row[row], xWins) || board.row[row].isEqual(yWins);
+        var winner = false; // remove after testing and reactivate line above that is currently commented out for testing
+        if (winner) {
+            return board.row[row][0]; // WE HAVE A WINNER. RETURN X OR O SO WE KNOW WHO WON.  ### MAY NEED TO TWEAK ###
+        }
     }
+    // ALTERNATE CODE FOR ARRAY EQUALITY COMPARISON IF I DIDN'T USE isEqual METHOD FROM THE UNDERSCORE LIBRARY
+    // function equalArrays(arr1, arr2) {
+    //    for (i = 0; i < arr1.length; i++) {
+    //        if (arr1[i] === arr2[i]) return true;
+    //    }
+    return winner;
 }
 
-// Print board function text diagonal
-for (row = 1; row < 4; row++) {
-    var thisRow = "row" + row;
-    var idx = row - 1;
-  //  console.log(row, board[thisRow][idx]);
+// FUNCTION TO TEST VERTICAL ROWS FOR A WINNER (TESTED AND WORKING)
+function testVert() {
+    var winner = false;
+    for (idx = 0; (idx < 3) && (winner === false); idx++) { // idx are array indexes for comparing columns with same index
+        winner = true // initialize to true so that loop below goes through initial iteration
+        for (xRow = 1; (xRow < 4) && (winner === true); xRow++) {
+            var thisRow = "row" + xRow;
+            winner = board[thisRow][idx] === "X" ? true : false; // testing for X winner
+        }
+        if (winner === true){return "X"};
+        winner = true;
+        for (row = 1; (row < 4) && (winner === true); row++) {
+            var thisRow = "row" + row;
+            winner = board[thisRow][idx] === "O" ? true : false; // testing for O winner
+        }
+        if (winner === true){return "O"};
+    }
+    return winner;
 }
 
-var idx = 3;//include in scope of function once tested and applied
-for (row = 1; row < 4; row++) {
-    var thisRow = "row" + row;
-    idx--;
-  //  console.log(row, board[thisRow][idx]);
+// FUNCTION TO TEST DIAGONAL ROWS FOR A WINNER (TESTED AND WORKING)
+function testDiag() {
+    var winner = true;
+    var gamePiece = "X"; // TEST FOR X WINNER FIRST
+    for (i = 1; i < 3; i++) { // LOOPS TWICE TO CHECK FOR X WINNER, THEN O WINNER
+        for (row = 1; (row < 4) && (winner === true); row++) {
+            var thisRow = "row" + row;
+            var idx = row - 1;
+            winner = board[thisRow][idx] === gamePiece ? true : false;
+        }
+        if (winner === true) {
+            return gamePiece
+        }
+
+        var idx = 3;
+        winner = true
+        for (row = 1; (row < 4) && (winner === true); row++) {
+            var thisRow = "row" + row;
+            idx--;
+            winner = board[thisRow][idx] === gamePiece ? true : false;
+        }
+        if (winner === true) {
+            return gamePiece
+        }
+        ;
+        gamePiece = "O"; // NOW LOOP AGAIN TO TEST FOR O WINNER
+    }
 }
 
 
 //  1. Who starts? User starts first time, winner starts first after that until game is exited
 //  2. player1 picks (call compPick function if it is computer pick)
-//  3. call status function to see if there is a winner yet
+//  3. call winner function to see if there is a winner yet
 //  4. player2 picks (call compPick function if it is computer pick)
-//  5. call status function to see if there is a winner yet
+//  5. call winner function to see if there is a winner yet
 //  6. New game or quit option (reset necessary values)
 
 
